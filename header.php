667bd98bc22427354ca5d8039db69676e88ab5fa
apple <apple@apples-MacBook-Air.local>
2024-09-20 08:50:14 -0700

    Defining Backend Structure

--- header.php
+++ header.php
@@ -16,13 +16,15 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
 <body>
-<?php wp_body_open(); ?>
+	<?php $header_content = get_field("header_group" , "options") ;  ?>
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
-		
+<!--  LOGO -->
+  <img src="<?php echo $header_content['logo']  ; ?>" alt="">
+  
+
 	</header><!-- #masthead -->
--- page-home.php
+++ page-home.php
@@ -1,1 +1,4 @@
+<?php get_header(;) ?>
 <h1> Welcome From Home Page</h1>
+
+<?php get_footer(); ?>
