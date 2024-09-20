78c8dd58a5c6b4c14cb0da7da14d7a30f631dcca
apple <apple@apples-MacBook-Air.local>
2024-09-20 08:35:49 -0700

    Defining Backend Structure

--- functions.php
+++ functions.php
@@ -1,1 +1,25 @@
 <?php 
+// Primary Theme options 
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Top Header',
+		'menu_title'	=> 'Top Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Header & Footer',
+		'menu_title'	=> 'Header & Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	
+}
