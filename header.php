7af6fc1dd3f13867952a8308ac0088969cb2a164
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 02:36:03 +0300

    styling nav

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <link href="<?php echo get_template_directory_uri() . '/assets/style/style.css'; ?>" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri() . './assets/css/output.css'; ?>" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
