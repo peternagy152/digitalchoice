5b64d14e3824999a560d0ce1dc3b51afc2d37acf
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 01:55:42 +0300

    styling nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{php,html,js}", "./*.{php,html,js}"],
+  content: [
+    "./*.php", // Scans PHP files in the root directory
+    "./**/*.php", // Scans PHP files in all subdirectories, except node_modules
+    "./assets/js/**/*.js", // Scans JavaScript files inside the assets/js folder
+    "./assets/style/**/*.css", // Scans CSS files inside the assets/style folder
+    "!./node_modules/**/*", // Excludes node_modules
+    "!./vendor/**/*", // Excludes vendor directories if using Composer
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#fa4223",
-        },
-        secondary: "#222222",
-        accent: "#3A96DB",
-        muted: "#AAAAAA",
-        buttonPrimary: "#001A70",
-        buttonMenu: { DEFAULT: "#FAC63E" },
-        buttonSecondary: "#2E86C9",
-        buttonPrimaryHover: "#002B96",
-        buttonMenuHover: "#FFD24D",
-        buttonSecondaryHover: "#3A9CE3",
-        menuColor: "#000F3D",
-        coolGray: "#EDF1F4",
-        monoBlack: "#1A1A1A",
-        dateColor: "#AAAAAA",
-        goldColor: "#B48965",
-        lightGoldColor: "#EFE3B3",
-      },
-
-      screens: {
-        xs: "501px",
-        xl: "1280px",
-        "1xl": "1300px",
-        "2xl": "1536px",
-        "3xl": "1536px",
-        "4xl": "1440px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
