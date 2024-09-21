92576bdcc6dc7aa4c5bb8fb7d28e5b6d9ec36c4e
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 10:44:56 +0300

    styling nav

--- header.php
+++ header.php
@@ -18,7 +18,24 @@
                 <img src="<?php echo $header_content['logo']; ?>" alt="" class="logo">
                 <span class="website-name">Digital <span class="highlight">Choice</span></span>
             </div>
-
+            <div class="menu_nav">
+                <?php foreach ($header_content["main_menu"] as $one_item) { ?>
+                <a class="menu-item" href="<?php echo $one_item['menu_item']['url']; ?>">
+                    <?php echo $one_item["menu_item"]['title']; ?>
+                </a>
+                <?php } ?>
+            </div>
+            <!-- Calendly Button -->
+            <a href="/calendy" class="calendly_button_nav">
+                <span class="icon">
+                    <svg class="icon-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M17.091 0.800049V3.85547H6.90959V0.800049H3.85417V3.85547H0.800781V23.2001H23.1987V3.85547H20.1454V0.800049H17.091ZM3.85417 8.94513H20.1443V6.90886H3.85417V8.94513ZM3.85417 20.1456H20.1443V11.9995H3.85417V20.1456Z"
+                            fill="CurrentColor"></path>
+                    </svg>
+                </span>
+                <span class="calendly-text">Calendly</span>
+            </a>
             <!-- Navigation Links -->
             <div class="nav-content">
                 <div class="logo_nav">
