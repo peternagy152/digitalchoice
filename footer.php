72e023281f1224aebfc7dfe72e3cba1eea3a6a2e
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 19:38:24 +0300

    New Slider

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-    <div class="bg-white dark:bg-gray-900">
+    <div class="bg-black ">
         <div class="container px-6 py-8 mx-auto">
             <div class="flex flex-col items-center text-center">
                 <a href="#">
@@ -20,37 +20,31 @@
                 </a>
 
                 <div class="flex flex-wrap justify-center mt-6 -mx-4">
-                    <a href="#"
-                        class="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+                    <a href="#" class="mx-4 text-sm text-white transition-colors duration-300 hover:text-primary  "
                         aria-label="Reddit"> Home </a>
 
-                    <a href="#"
-                        class="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+                    <a href="#" class="mx-4 text-sm text-white transition-colors duration-300 hover:text-primary  "
                         aria-label="Reddit"> About </a>
 
-                    <a href="#"
-                        class="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+                    <a href="#" class="mx-4 text-sm text-white transition-colors duration-300 hover:text-primary  "
                         aria-label="Reddit"> Teams </a>
 
-                    <a href="#"
-                        class="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+                    <a href="#" class="mx-4 text-sm text-white transition-colors duration-300 hover:text-primary  "
                         aria-label="Reddit"> Privacy </a>
 
-                    <a href="#"
-                        class="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400"
+                    <a href="#" class="mx-4 text-sm text-white transition-colors duration-300 hover:text-primary  "
                         aria-label="Reddit"> Cookies </a>
                 </div>
 
             </div>
 
-            <hr class="my-6 border-gray-200 md:my-10 dark:border-gray-700" />
+            <hr class="my-6 border-gray-200 md:my-10 " />
 
             <div class="flex flex-col items-center sm:flex-row sm:justify-between">
-                <p class="text-sm text-gray-500 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
+                <p class="text-sm text-white ">© Copyright 2021. All Rights Reserved.</p>
 
                 <div class="flex -mx-2">
-                    <a href="#"
-                        class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    <a href="#" class="mx-2 text-white transition-colors duration-300 hover:text-primary"
                         aria-label="Reddit">
                         <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
@@ -60,8 +54,7 @@
                         </svg>
                     </a>
 
-                    <a href="#"
-                        class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    <a href="#" class="mx-2 text-white transition-colors duration-300 hover:text-primary"
                         aria-label="Facebook">
                         <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
@@ -71,8 +64,7 @@
                         </svg>
                     </a>
 
-                    <a href="#"
-                        class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    <a href="#" class="mx-2 text-white transition-colors duration-300 hover:text-primary"
                         aria-label="Github">
                         <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
