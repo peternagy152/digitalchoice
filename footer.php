2a4f8c12db8ae82b0b68f20f2392c02b2cbc8671
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 18:15:25 +0300

    New Hero

--- footer.php
+++ footer.php
@@ -11,26 +11,29 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digitalchoice' ) ); ?>">
-				<?php
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digitalchoice' ) ); ?>">
+            <?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'digitalchoice' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
+        </a>
+        <span class="sep"> | </span>
+        <?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'digitalchoice' ), 'digitalchoice', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    </div><!-- .site-info -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+</footer><!-- #colophon -->
 </div><!-- #page -->
-<script href="<?php echo get_template_directory_uri() . '/assets/js/main.js'; ?>" ></script>
+<script href="<?php echo get_template_directory_uri() . '/assets/js/main.js'; ?>"></script>
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
