004c4160ceb0f8d378885ebc40a9e113b1830a01
KareemTarek <kkareemtarek2@gmail.com>
2024-09-21 10:27:30 +0300

    styling nav

--- header.php
+++ header.php
@@ -20,8 +20,13 @@
                 <span class="website-name">Digital <span class="highlight">Choice</span></span>
             </div>
 
-            <!-- Navigation Links for Desktop -->
+            <!-- Navigation Links -->
             <div class="nav-content">
+                <div class="logo_nav">
+                    <img src="<?php echo $header_content['logo']; ?>" alt="" class="logo">
+                    <span class="website-name">Digital <span class="highlight">Choice</span></span>
+                </div>
+                <div class="borderline"></div>
                 <div class="menu">
                     <?php foreach ($header_content["main_menu"] as $one_item) { ?>
                     <a class="menu-item" href="<?php echo $one_item['menu_item']['url']; ?>">
@@ -29,6 +34,10 @@
                     </a>
                     <?php } ?>
                 </div>
+                <div class="borderline"></div>
+
+                <!-- Close Button -->
+                <button class="close-button" onclick="toggleNav()">X</button>
 
                 <!-- Calendly Button -->
                 <a href="/calendy" class="calendly-button">
@@ -49,43 +58,6 @@
                 <span class="bar"></span>
                 <span class="bar"></span>
             </div>
-        </div>
-
-        <!-- Mobile/Tablet Slider Content -->
-        <div class="slider">
-            <!-- Close Button -->
-            <button class="close-button" onclick="toggleNav()">X</button>
-
-            <!-- Logo and Website Name on Top in Slider -->
-            <div class="slider-logo-container">
-                <img src="<?php echo $header_content['logo']; ?>" alt="" class="slider-logo">
-                <span class="slider-website-name">Digital <span class="highlight">Choice</span></span>
-                <div class="slider-divider"></div>
-            </div>
-
-            <!-- Navigation Links inside Slider -->
-            <div class="slider-menu">
-                <?php foreach ($header_content["main_menu"] as $one_item) { ?>
-                <a class="menu-item" href="<?php echo $one_item['menu_item']['url']; ?>">
-                    <?php echo $one_item["menu_item"]['title']; ?>
-                </a>
-                <?php } ?>
-            </div>
-
-            <!-- Divider below menu -->
-            <div class="slider-divider"></div>
-
-            <!-- Calendly Button inside Slider -->
-            <a href="/calendy" class="calendly-button">
-                <span class="icon">
-                    <svg class="icon-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M17.091 0.800049V3.85547H6.90959V0.800049H3.85417V3.85547H0.800781V23.2001H23.1987V3.85547H20.1454V0.800049H17.091ZM3.85417 8.94513H20.1443V6.90886H3.85417V8.94513ZM3.85417 20.1456H20.1443V11.9995H3.85417V20.1456Z"
-                            fill="CurrentColor"></path>
-                    </svg>
-                </span>
-                <span class="calendly-text">Calendly</span>
-            </a>
         </div>
     </nav>
 
